3e52a846bcf70cfd5853d1598d5cf6c8a5103bf4
Daniel Riissanen <daniel.riissanen@gmail.com>
2019-01-10 14:20:47 +0200

    Add initial project files

---
+++ desktop/src/fi/chop/desktop/DesktopLauncher.java
@@ -0,0 +1,12 @@
+package fi.chop.desktop;
+
+import com.badlogic.gdx.backends.lwjgl.LwjglApplication;
+import com.badlogic.gdx.backends.lwjgl.LwjglApplicationConfiguration;
+import fi.chop.Chop;
+
+public class DesktopLauncher {
+	public static void main (String[] arg) {
+		LwjglApplicationConfiguration config = new LwjglApplicationConfiguration();
+		new LwjglApplication(new Chop(), config);
+	}
+}
