6197181122aef0d403228dc419c52f94e8728121
Daniel Riissanen <daniel.riissanen@gmail.com>
2019-01-11 13:32:59 +0200

    Add PowerMeter

---
+++ core/src/fi/chop/model/PowerMeter.java
@@ -0,0 +1,20 @@
+package fi.chop.model;
+
+public class PowerMeter {
+
+    private float value;
+
+    public void set(float value) {
+        if (value < 0 || value > 1)
+            throw new IllegalArgumentException("value must be between 0 and 1");
+        this.value = value;
+    }
+
+    public void add(float delta) {
+        this.value = Math.min(Math.max(value + delta, 0), 1);
+    }
+
+    public float getFillPercentage() {
+        return value;
+    }
+}
---
+++ core/test/fi/chop/model/TestPowerMeter.java
@@ -0,0 +1,71 @@
+package fi.chop.model;
+
+import org.junit.Before;
+import org.junit.Test;
+
+import static org.junit.Assert.assertEquals;
+
+public class TestPowerMeter {
+
+    private static final float EPSILON = 0.000001f;
+
+    private PowerMeter meter;
+
+    @Before
+    public void setUp() {
+        meter = new PowerMeter();
+    }
+
+    @Test
+    public void testDefaultValues() {
+        assertEquals(0, meter.getFillPercentage(), EPSILON);
+    }
+
+    @Test
+    public void testSet() {
+        meter.set(0.2f);
+        assertEquals(0.2f, meter.getFillPercentage(), EPSILON);
+    }
+
+    @Test(expected = IllegalArgumentException.class)
+    public void testSetNegative() {
+        meter.set(-0.1f);
+    }
+
+    @Test(expected = IllegalArgumentException.class)
+    public void testSetOverOne() {
+        meter.set(1.1f);
+    }
+
+    @Test
+    public void testAddPositive() {
+        meter.set(0.2f);
+        meter.add(0.3f);
+        assertEquals(0.5f, meter.getFillPercentage(), EPSILON);
+    }
+
+    @Test
+    public void testAddNegative() {
+        meter.set(0.5f);
+        meter.add(-0.2f);
+        assertEquals(0.3f, meter.getFillPercentage(), EPSILON);
+    }
+
+    @Test
+    public void testAddResultNegative() {
+        meter.add(-0.2f);
+        assertEquals(0, meter.getFillPercentage(), EPSILON);
+        meter.set(0.1f);
+        meter.add(-0.2f);
+        assertEquals(0, meter.getFillPercentage(), EPSILON);
+    }
+
+    @Test
+    public void testAddResultOverOne() {
+        meter.add(1.1f);
+        assertEquals(1, meter.getFillPercentage(), EPSILON);
+        meter.set(0.8f);
+        meter.add(0.3f);
+        assertEquals(1, meter.getFillPercentage(), EPSILON);
+    }
+}
