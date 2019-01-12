268f1d549e920d70ed3a2d2057296a3cd1abf531
Daniel Riissanen <daniel.riissanen@gmail.com>
2019-01-12 11:30:33 +0200

    Add instruction text to MainMenuScreen

--- core/src/fi/chop/screens/MainMenuScreen.java
+++ core/src/fi/chop/screens/MainMenuScreen.java
@@ -1,11 +1,17 @@
 package fi.chop.screens;
 
 import com.badlogic.gdx.Gdx;
+import com.badlogic.gdx.graphics.g2d.BitmapFont;
+import com.badlogic.gdx.graphics.g2d.GlyphLayout;
 import com.badlogic.gdx.graphics.g2d.SpriteBatch;
 import fi.chop.Chop;
 import fi.chop.input.MainMenuScreenInput;
 
 public class MainMenuScreen extends ChopScreen {
+
+    private BitmapFont font;
+    private GlyphLayout layout;
+    private String text;
 
     public MainMenuScreen(Chop game) {
         super(game);
@@ -14,11 +20,22 @@ public class MainMenuScreen extends ChopScreen {
     @Override
     public void show() {
         Gdx.input.setInputProcessor(new MainMenuScreenInput(this, getInputMap()));
+
+        text = "Press [Space] to start";
+        font = getAssets().get("fonts/ZCOOL-Regular.ttf", BitmapFont.class);
+        layout = new GlyphLayout(font, text);
     }
 
     @Override
     protected void update(float delta) { }
 
     @Override
-    protected void render(SpriteBatch batch) { }
+    protected void render(SpriteBatch batch) {
+        beginRender();
+        batch.begin();
+        float drawX = getCamera().viewportWidth / 2 - layout.width / 2;
+        float drawY = getCamera().viewportHeight / 2 - layout.height / 2;
+        font.draw(batch, text, drawX, drawY);
+        batch.end();
+    }
 }
