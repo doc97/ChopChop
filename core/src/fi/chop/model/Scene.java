99e1d12896ccd96f3c54d13b60af81c58c08d834
Daniel Riissanen <daniel.riissanen@gmail.com>
2019-01-13 19:28:18 +0200

    Add Layer wrapper functions to Scene

--- core/src/fi/chop/model/Scene.java
+++ core/src/fi/chop/model/Scene.java
@@ -1,11 +1,13 @@
 package fi.chop.model;
 
 import com.badlogic.gdx.graphics.g2d.SpriteBatch;
+import fi.chop.model.object.GameObject;
 
 import java.util.ArrayList;
 import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
+import java.util.function.Predicate;
 
 public class Scene {
 
@@ -15,6 +17,82 @@ public class Scene {
     public Scene() {
         names = new ArrayList<>();
         layers = new HashMap<>();
+    }
+
+    public void addObjects(String layerName, GameObject... objects) {
+        if (!names.contains(layerName))
+            throw new IllegalArgumentException("No layer with name: " + layerName);
+        layers.get(layerName).add(objects);
+    }
+
+    public GameObject findOne(Predicate<GameObject> predicate) {
+        for (String name : names) {
+            GameObject result = layerFindOne(name, predicate);
+            if (result != null)
+                return result;
+        }
+        return null;
+    }
+
+    public GameObject layerFindOne(String layerName, Predicate<GameObject> predicate) {
+        if (!names.contains(layerName))
+            throw new IllegalArgumentException("No layer with name: " + layerName);
+        return layers.get(layerName).findOne(predicate);
+    }
+
+    public <T extends GameObject> T findOne(Class<T> clazz) {
+        for (String name : names) {
+            T result = layerFindOne(name, clazz);
+            if (result != null)
+                return result;
+        }
+        return null;
+    }
+
+    public <T extends GameObject> T layerFindOne(String layerName, Class<T> clazz) {
+        if (!names.contains(layerName))
+            throw new IllegalArgumentException("No layer with name: " + layerName);
+        return layers.get(layerName).findOne(clazz);
+    }
+
+    public List<GameObject> findAll(Predicate<GameObject> predicate) {
+        List<GameObject> result = new ArrayList<>();
+        for (String name : names) {
+            List<GameObject> subResult = layerFindAll(name, predicate);
+            result.addAll(subResult);
+        }
+        return result;
+    }
+
+    public List<GameObject> layerFindAll(String layerName, Predicate<GameObject> predicate) {
+        if (!names.contains(layerName))
+            throw new IllegalArgumentException("No layer with name: " + layerName);
+        return layers.get(layerName).findAll(predicate);
+    }
+
+    public <T extends GameObject> List<T> findAll(Class<T> clazz) {
+        List<T> result = new ArrayList<>();
+        List<GameObject> objects = findAll(clazz::isInstance);
+        for (GameObject obj : objects)
+            result.add(clazz.cast(obj));
+        return result;
+    }
+
+    public <T extends GameObject> List<T> layerFindAll(String layerName, Class<T> clazz) {
+        if (!names.contains(layerName))
+            throw new IllegalArgumentException("No layer with name: " + layerName);
+        return layers.get(layerName).findAll(clazz);
+    }
+
+    public void killAll(Predicate<GameObject> predicate) {
+        for (String name : names)
+            layers.get(name).killAll(predicate);
+    }
+
+    public void layerKillAll(String layerName, Predicate<GameObject> predicate) {
+        if (!names.contains(layerName))
+            throw new IllegalArgumentException("No layer with name: " + layerName);
+        layers.get(layerName).killAll(predicate);
     }
 
     public void update(float delta) {
