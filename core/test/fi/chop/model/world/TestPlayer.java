775736d0a085371d4a1d05626a910c3090985012
Daniel Riissanen <daniel.riissanen@gmail.com>
2019-01-18 20:12:57 +0200

    Add tests for event notification in Player

--- core/test/fi/chop/model/world/TestPlayer.java
+++ core/test/fi/chop/model/world/TestPlayer.java
@@ -1,18 +1,34 @@
 package fi.chop.model.world;
 
+import fi.chop.event.EventData;
+import fi.chop.event.EventListener;
+import fi.chop.event.EventSystem;
+import fi.chop.event.Events;
 import org.junit.Before;
 import org.junit.Test;
 
 import static org.junit.Assert.*;
 
-public class TestPlayer {
+public class TestPlayer implements EventListener {
 
     private static final float EPSILON = 0.000001f;
+    private EventSystem eventSystem;
     private Player player;
+
+    private int reputationLvl;
+    private float reputation;
+    private float popularity;
 
     @Before
     public void setUp() {
-        player = new Player(null);
+        eventSystem = new EventSystem();
+        player = new Player(eventSystem);
+        eventSystem.addListener(this, Events.EVT_POPULARITY_CHANGED,
+                Events.EVT_REPUTATION_CHANGED, Events.EVT_REPUTATION_LVL_CHANGED);
+
+        reputationLvl = 1;
+        reputation = 0;
+        popularity = 0;
     }
 
     @Test
@@ -54,91 +70,127 @@ public class TestPlayer {
     @Test
     public void testAddPopularityZero() {
         player.addPopularity(0);
+        eventSystem.update();
         assertEquals(0, player.getPopularity(), EPSILON);
+        assertEquals(0, popularity, EPSILON);
     }
 
     @Test
     public void testAddPopularityPositive() {
         player.addPopularity(0.2f);
+        eventSystem.update();
         assertEquals(0.2f, player.getPopularity(), EPSILON);
+        assertEquals(0.2f, popularity, EPSILON);
         player.addPopularity(0.2f);
+        eventSystem.update();
         assertEquals(0.4f, player.getPopularity(), EPSILON);
+        assertEquals(0.4f, popularity, EPSILON);
     }
 
     @Test
     public void testAddPopularityNegative() {
         player.addPopularity(0.8f);
         player.addPopularity(-0.2f);
+        eventSystem.update();
         assertEquals(0.6f, player.getPopularity(), EPSILON);
+        assertEquals(0.6f, popularity, EPSILON);
         player.addPopularity(-0.1f);
+        eventSystem.update();
         assertEquals(0.5f, player.getPopularity(), EPSILON);
+        assertEquals(0.5f, popularity, EPSILON);
     }
 
     @Test
     public void testAddPopularityBelowZero() {
         player.addPopularity(-0.1f);
+        eventSystem.update();
         assertEquals(0, player.getPopularity(), EPSILON);
+        assertEquals(0, popularity, EPSILON);
     }
 
     @Test
     public void testAddPopularityAboveOne() {
         player.addPopularity(0.8f);
         player.addPopularity(0.3f);
+        eventSystem.update();
         assertEquals(1, player.getPopularity(), EPSILON);
+        assertEquals(1, popularity, EPSILON);
     }
 
     @Test
     public void testAddReputationZero() {
         player.addReputation(0);
+        eventSystem.update();
         assertEquals(0, player.getReputation(), EPSILON);
+        assertEquals(0, reputation, EPSILON);
     }
 
     @Test
     public void testAddReputationPositive() {
         player.addReputation(0.2f);
+        eventSystem.update();
         assertEquals(0.2f, player.getReputation(), EPSILON);
+        assertEquals(0.2f, reputation, EPSILON);
         player.addReputation(0.2f);
+        eventSystem.update();
         assertEquals(0.4f, player.getReputation(), EPSILON);
+        assertEquals(0.4f, reputation, EPSILON);
     }
 
     @Test
     public void testAddReputationNegative() {
         player.addReputation(0.8f);
         player.addReputation(-0.2f);
+        eventSystem.update();
         assertEquals(0.6f, player.getReputation(), EPSILON);
+        assertEquals(0.6f, reputation, EPSILON);
         player.addReputation(-0.1f);
+        eventSystem.update();
         assertEquals(0.5f, player.getReputation(), EPSILON);
+        assertEquals(0.5f, reputation, EPSILON);
     }
 
     @Test
     public void testAddReputationBelowZero() {
         player.addReputation(2.1f);
         player.addReputation(-0.2f);
+        eventSystem.update();
         assertEquals(0.9f, player.getReputation(), EPSILON);
+        assertEquals(0.9f, reputation, EPSILON);
         assertEquals(1, player.getReputationLevel());
+        assertEquals(1, reputationLvl, EPSILON);
     }
 
     @Test
     public void testAddReputationAboveOne() {
         player.addReputation(0.8f);
         player.addReputation(0.3f);
+        eventSystem.update();
         assertEquals(0.1f, player.getReputation(), EPSILON);
+        assertEquals(0.1f, reputation, EPSILON);
         assertEquals(1, player.getReputationLevel());
+        assertEquals(1, reputationLvl, EPSILON);
     }
 
     @Test
     public void testIncreaseReputationLevel() {
         player.addReputation(1);
+        eventSystem.update();
         assertEquals(0, player.getReputation(), EPSILON);
+        assertEquals(0, reputation, EPSILON);
         assertEquals(1, player.getReputationLevel());
+        assertEquals(1, reputationLvl, EPSILON);
     }
 
     @Test
     public void testDecreaseReputationLevel() {
         player.addReputation(2);
         player.addReputation(-1);
+        eventSystem.update();
         assertEquals(0, player.getReputation(), EPSILON);
+        assertEquals(0, reputation, EPSILON);
         assertEquals(1, player.getReputationLevel());
+        assertEquals(1, reputationLvl, EPSILON);
     }
 
     @Test
@@ -190,5 +242,20 @@ public class TestPlayer {
         assertTrue(player.hasEnoughMoney(8));
         assertTrue(player.hasEnoughMoney(10));
         assertFalse(player.hasEnoughMoney(11));
+    }
+
+    @Override
+    public void handle(Events event, EventData data) {
+        switch (event) {
+            case EVT_POPULARITY_CHANGED:
+                popularity = (float) data.get();
+                break;
+            case EVT_REPUTATION_CHANGED:
+                reputation = (float) data.get();
+                break;
+            case EVT_REPUTATION_LVL_CHANGED:
+                reputationLvl = (int) data.get();
+                break;
+        }
     }
 }
