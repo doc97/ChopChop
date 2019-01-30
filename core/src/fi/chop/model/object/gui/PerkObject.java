2b3a2f9997cfe7f5627ecc540bfd3287bdb5a7dd
Daniel Riissanen <daniel.riissanen@gmail.com>
2019-01-31 01:42:50 +0200

    Add tooltips to perk icons

--- core/src/fi/chop/model/object/gui/PerkObject.java
+++ core/src/fi/chop/model/object/gui/PerkObject.java
@@ -3,10 +3,9 @@ package fi.chop.model.object.gui;
 import com.badlogic.gdx.assets.AssetManager;
 import com.badlogic.gdx.graphics.OrthographicCamera;
 import com.badlogic.gdx.graphics.g2d.SpriteBatch;
-import com.badlogic.gdx.graphics.g2d.TextureAtlas;
-import com.badlogic.gdx.graphics.g2d.TextureRegion;
-import fi.chop.engine.DrawParameters;
+import fi.chop.model.auxillary.Align;
 import fi.chop.model.object.GameObject;
+import fi.chop.model.object.util.TextureRegionObject;
 import fi.chop.model.world.Player;
 import fi.chop.model.world.PopularityPerk;
 import fi.chop.model.world.WorldState;
@@ -15,12 +14,9 @@ public class PerkObject extends GameObject {
 
     private static final int SPACING_X_PX = 10;
 
-    private TextureRegion eye;
-    private TextureRegion gift;
-    private TextureRegion heart;
-    private DrawParameters eyeParams;
-    private DrawParameters giftParams;
-    private DrawParameters heartParams;
+    private TextureRegionObject eye;
+    private TextureRegionObject gift;
+    private TextureRegionObject heart;
 
     public PerkObject(AssetManager assets, OrthographicCamera camera, WorldState world, Player player) {
         super(assets, camera, world, player);
@@ -28,42 +24,85 @@ public class PerkObject extends GameObject {
 
     @Override
     public void load() {
-        TextureAtlas atlas = getAssets().get("textures/packed/Chop.atlas", TextureAtlas.class);
-        eye = atlas.findRegion("ic-perk-turning-a-blind-eye");
-        gift = atlas.findRegion("ic-perk-gift-of-the-people");
-        heart = atlas.findRegion("ic-perk-meaningful-work");
+        String atlasName = "textures/packed/Chop.atlas";
+        eye = loadTextureRegionObject(atlasName, "ic-perk-turning-a-blind-eye",
+                "You won't lose popularity during the next execution");
+        gift = loadTextureRegionObject(atlasName, "ic-perk-gift-of-the-people",
+                "If you follow the crowd's opinion, your earnings will be doubled");
+        heart = loadTextureRegionObject(atlasName, "ic-perk-meaningful-work",
+                "The next execution will be easier to perform");
 
-        eyeParams = new DrawParameters(eye);
-        giftParams = new DrawParameters(gift);
-        heartParams = new DrawParameters(heart);
-
-        getTransform().setSize(
-                eyeParams.width + giftParams.width + heartParams.width + 2 * SPACING_X_PX,
-                Math.max(Math.max(eyeParams.height, giftParams.height), heartParams.height));
+        float totalWidth = eye.getTransform().getScaledWidth() + gift.getTransform().getScaledWidth() +
+                heart.getTransform().getScaledWidth() + 2 * SPACING_X_PX;
+        float totalHeight = Math.max(
+                Math.max(eye.getTransform().getScaledHeight(), gift.getTransform().getScaledHeight()),
+                heart.getTransform().getScaledHeight()
+        );
+        getTransform().setSize(totalWidth, totalHeight);
     }
 
     @Override
-    public void update(float delta) { }
-
-    @Override
-    public void render(SpriteBatch batch) {
-        float offset = 0;//getTransform().getX();
+    public void update(float delta) {
+        float offset = 0;
         if (getPlayer().hasPerk(PopularityPerk.TURNING_A_BLIND_EYE)) {
-            eyeParams.x = offset;
-            draw(batch, eye, eyeParams);
-            offset -= eyeParams.width + SPACING_X_PX;
+            if (!eye.isEnabled(Toggles.RENDER)) {
+                eye.toggleOn(Toggles.RENDER);
+                eye.getTransform().setX(offset);
+                offset -= eye.getTransform().getScaledWidth() + SPACING_X_PX;
+            }
+        } else {
+            eye.toggleOff(Toggles.RENDER);
         }
+
         if (getPlayer().hasPerk(PopularityPerk.GIFT_OF_THE_PEOPLE)) {
-            giftParams.x = offset;
-            draw(batch, gift, giftParams);
-            offset -= giftParams.width + SPACING_X_PX;
+            if (!gift.isEnabled(Toggles.RENDER)) {
+                gift.toggleOn(Toggles.RENDER);
+                gift.getTransform().setX(offset);
+                offset -= gift.getTransform().getScaledWidth() + SPACING_X_PX;
+            }
+        } else {
+            gift.toggleOff(Toggles.RENDER);
         }
+
         if (getPlayer().hasPerk(PopularityPerk.MEANINGFUL_WORK)) {
-            heartParams.x = offset;
-            draw(batch, heart, heartParams);
+            if (!heart.isEnabled(Toggles.RENDER)) {
+                heart.toggleOn(Toggles.RENDER);
+                heart.getTransform().setX(offset);
+            }
+        } else {
+            heart.toggleOff(Toggles.RENDER);
         }
     }
+
+    @Override
+    public void render(SpriteBatch batch) { }
 
     @Override
     public void dispose() { }
+
+    @Override
+    public void die() {
+        super.die();
+        eye.die();
+        gift.die();
+        heart.die();
+    }
+
+    @Override
+    public GameObject[] getChildren() {
+        return new GameObject[] { eye, gift, heart };
+    }
+
+    private TextureRegionObject loadTextureRegionObject(String atlasName, String regionName, String tooltip) {
+        TextureRegionObject object = new TextureRegionObject(getAssets(), getCamera(), getWorld(), getPlayer());
+        object.setTooltip(tooltip);
+        object.getTransform().setParent(getTransform());
+        object.getTransform().setAlign(Align.TOP_RIGHT);
+        object.getTransform().setOrigin(1, 1);
+        object.setRegion(atlasName, regionName);
+        object.load();
+        // Required to update position for the first time
+        object.toggleOff(Toggles.RENDER);
+        return object;
+    }
 }
